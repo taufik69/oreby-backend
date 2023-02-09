3fa08a241cff47518b4d5198abdda59300c4cb63
taufik69 <taufik35-2915@diu.edu.bd>
2023-02-09 19:23:59 +0600

    add new routes

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ app.get("/", (req, res) => {
     message: "This is root route",
   });
 });
+app.get("/info", (req, res) => {
+  res.status(200).json([
+    {
+      name: "Taufik islalm",
+    },
+    {
+      name: "Allauddin mondol",
+    },
+    {
+      name: "Thamina begum",
+    },
+    {
+      name: "Memi akter Moni",
+    },
+    {
+      name: "Oliul islam sojib",
+    },
+  ]);
+});
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(chalk.bgGreenBright(`Port running on ${process.env.PORT}`));
